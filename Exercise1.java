3c852509c37286b984e4d5aed6049f734faa607b
JyothiYenduri <120366268+JyothiYenduri@users.noreply.github.com>
2023-01-05 20:04:09 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,9 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+class Exercise1 {
+  public static void main(String args[]){
+    System.out.println("Name:Jyothi Yenduri");
+    System.out.println("Father's Name:Subramanyeswara Rao Yenduri");
+    System.out.println("Mail ID:jyothiyenduri10@gmail.com");
+  }
+} 
